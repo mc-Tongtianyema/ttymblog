c52d5de04e007377a6f34a94ecc5218f8bc98a7a
mc-Tongtianyema <3824181006@qq.com>
2025-12-31 21:18:00 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: '通天野马博客',
   // 网站地址
-  Site: 'https://www.vvhan.com',
+  Site: 'ttym1.tbit.xin/ttymblog',
   // 网站副标题
-  Subtitle: '不曾与你分享的时间,我在进步.',
+  Subtitle: '走在数字世界的前面.',
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description: 'Minecraft爱好者&不知名开发者，擅长使用c++,java,html等语言开发。',
   // 网站作者
-  Author: '.𝙃𝙖𝙣',
+  Author: '通天野马',
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Avatar: '',
   // 网站座右铭
   Motto: '运气是计划之外的东西.',
   // Cover 网站缩略图
@@ -23,7 +23,7 @@ export default {
     "I am making progress in the time I haven't shared with you.",
   ],
   // 网站创建时间
-  CreateTime: '2021-09-01',
+  CreateTime: '2025-12-31',
   // 顶部 Banner 配置
   HomeBanner: {
     enable: true,
@@ -64,11 +64,8 @@ export default {
   WebSites: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
-    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
-    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
-    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
+    { text: 'Github', link: 'https://github.com/mc-Tongtianyema', icon: 'WebSite_github' },
+    { text: '路过图床', link: 'https://imgse.com', icon: 'WebSite_img' },
   ],
   // 侧边栏展示
   AsideShow: {
